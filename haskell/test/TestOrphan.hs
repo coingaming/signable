9ccf824180cc44886b6fc81f06d319047bd59125
Ilja Tkachuk <tkachuk.labs@gmail.com>
2020-09-07 12:33:18 +0300

    TH impl WIP

--- haskell/test/TestOrphan.hs
+++ haskell/test/TestOrphan.hs
@@ -10,9 +10,13 @@ import Data.Signable
 import Data.Signable.Import hiding (show)
 import Proto.GoogleProtobuf
 import Proto.GoogleProtobuf_Fields
+import Proto.Schema
+import Proto.Schema_Fields
 import Prelude (Show (..))
 
 instance Show Sig where
   show = show . exportSig
 
 $(mkSignable $(mkProxy "Int32Value"))
+
+$(mkSignable $(mkProxy "Data0"))
