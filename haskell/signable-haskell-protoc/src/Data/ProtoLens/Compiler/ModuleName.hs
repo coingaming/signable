46a6245c852b40dc69b12c1183906213e1aef392
Ilja Tkachuk <tkachuk.labs@gmail.com>
2020-09-14 00:22:16 +0300

    protoc plugin WIP

--- haskell/signable-haskell-protoc/src/Data/ProtoLens/Compiler/ModuleName.hs
+++ haskell/signable-haskell-protoc/src/Data/ProtoLens/Compiler/ModuleName.hs
@@ -1,5 +1,7 @@
 module Data.ProtoLens.Compiler.ModuleName
-    ( protoModuleName ) where
+  ( protoModuleName,
+  )
+where
 
 import Data.Char (toUpper)
 import Data.List (intercalate)
@@ -11,14 +13,16 @@ protoModuleName path = fixModuleName rawModuleName
   where
     fixModuleName "" = ""
     -- Characters allowed in Bazel filenames but not in module names:
-    fixModuleName ('.':c:cs) = '.' : toUpper c : fixModuleName cs
-    fixModuleName ('_':c:cs) = toUpper c : fixModuleName cs
-    fixModuleName ('-':c:cs) = toUpper c : fixModuleName cs
-    fixModuleName (c:cs) = c : fixModuleName cs
-    rawModuleName = intercalate "."
-                        . (prefix :)
-                        . splitDirectories $ dropExtension
-                        $ path
+    fixModuleName ('.' : c : cs) = '.' : toUpper c : fixModuleName cs
+    fixModuleName ('_' : c : cs) = toUpper c : fixModuleName cs
+    fixModuleName ('-' : c : cs) = toUpper c : fixModuleName cs
+    fixModuleName (c : cs) = c : fixModuleName cs
+    rawModuleName =
+      intercalate "."
+        . (prefix :)
+        . splitDirectories
+        $ dropExtension
+        $ path
 
 prefix :: String
 prefix = "Proto"
