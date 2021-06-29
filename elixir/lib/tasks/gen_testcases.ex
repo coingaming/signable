3bcd5286d623cdbf811ec26bcbf82daada94e2ab
tkachuk-labs <tkachuk.labs@gmail.com>
2021-06-29 18:47:21 +0300

    WIP

--- elixir/lib/tasks/gen_testcases.ex
+++ elixir/lib/tasks/gen_testcases.ex
@@ -1,5 +1,6 @@
 defmodule Mix.Tasks.GenTestcases do
   use Mix.Task
+  require Number.Payload
 
   @impl Mix.Task
   def run([tc_file]) do
@@ -15,27 +16,134 @@ defmodule Mix.Tasks.GenTestcases do
       |> :public_key.pem_entry_decode()
       |> elem(2)
 
-    new_testcases = Enum.flat_map(0..1000, fn ->
-      Enum.map(test_config["testcases"], fn tc ->
-        proto_mod = String.to_atom("Elixir." <> tc["proto_message_type"])
+    generators =
+      [
+        Number.Payload
+      ]
+      |> Enum.map(fn proto_mod ->
+        quote do
+          gen all(
+                name <- binary(),
+                email <- email_generator
+              ) do
+            %User{name: name, email: email}
+          end
+        end
 
-        proto_message = gen_proto_message(proto_mod)
+        %Protobuf.MessageProps{field_props: props, oneof: oneof_list} =
+          proto_mod.__message_props__()
 
-        signable_serialized = Signable.serialize(proto_message)
-        signature = Signable.sign(signable_serialized, private_key, curve)
+        oneof_map =
+          oneof_list
+          |> Enum.reduce(%{}, fn {k, v}, acc -> Map.put(acc, v, k) end)
 
-        tc
-          |> Map.put("signable_serialized_b64", Base.encode64(signable_serialized))
-          |> Map.put("signable_signature_b64", Base.encode64(signature))
+        proto_struct = proto_mod.new()
+
+        prop_names =
+          proto_struct
+          |> Map.from_struct()
+          |> Map.keys()
+
+        gen_binds =
+          prop_names
+          |> Enum.map(fn prop_name ->
+            %Protobuf.FieldProps{type: type} =
+              props
+              |> Map.values()
+              |> Enum.find(fn %Protobuf.FieldProps{name_atom: name_atom} ->
+                prop_name == name_atom
+              end)
+
+            prop_gen_ast =
+              case type do
+                :int32 ->
+                  quote do
+                    StreamData.integer()
+                    |> StreamData.filter(&(&1 > -2_147_483_648 and &1 < 2_147_483_648))
+                  end
+
+                :int64 ->
+                  quote do
+                    StreamData.integer(-9_223_372_036_854_775_808..9_223_372_036_854_775_808)
+                  end
+
+                :uint32 ->
+                  quote do
+                    StreamData.integer(0..4_294_967_295)
+                  end
+
+                :uint64 ->
+                  quote do
+                    StreamData.integer(0..18_446_744_073_709_551_615)
+                  end
+              end
+
+            {:<-, [],
+             [
+               {prop_name, [], Elixir},
+               prop_gen_ast
+             ]}
+          end)
+
+        gen_struct =
+          prop_names
+          |> Enum.map(fn prop_name ->
+            {prop_name, {prop_name, [], Elixir}}
+          end)
+
+        xs =
+          proto_mod
+          |> Module.split()
+          |> Enum.map(&String.to_atom/1)
+
+        {:gen, [context: Elixir, import: ExUnitProperties],
+         [
+           {:all, [], gen_binds},
+           [
+             do:
+               {:%, [],
+                [
+                  {:__aliases__, [alias: false], xs},
+                  {:%{}, [], gen_struct}
+                ]}
+           ]
+         ]}
+        |> Code.eval_quoted()
+        |> elem(0)
+
+        # quote do
+        #  ExUnitProperties.gen all name <- StreamData.string(:alphanumeric),
+        #                           domain <- StreamData.member_of(domains) do
+        #    name <> "@" <> domain
+        #  end
+        # end
       end)
+
+    Enum.each(generators, fn x ->
+      Enum.take(x, 100)
+      |> IO.inspect()
     end)
 
-    File.write!(tc_file, Poison.encode!(Map.put(test_config, "testcases", new_testcases), pretty: true))
+    # new_testcases = Enum.flat_map(0..1000, fn ->
+    #  Enum.map(test_config["testcases"], fn tc ->
+    #    proto_mod = String.to_atom("Elixir." <> tc["proto_message_type"])
+
+    #    proto_message = gen_proto_message(proto_mod)
+
+    #    signable_serialized = Signable.serialize(proto_message)
+    #    signature = Signable.sign(signable_serialized, private_key, curve)
+
+    #    tc
+    #      |> Map.put("signable_serialized_b64", Base.encode64(signable_serialized))
+    #      |> Map.put("signable_signature_b64", Base.encode64(signature))
+    #  end)
+    # end)
+
+    # File.write!(tc_file, Poison.encode!(Map.put(test_config, "testcases", new_testcases), pretty: true))
   end
 
   defp gen_proto_message(proto_mod) do
-    %Protobuf.MessageProps{field_props: props, oneof: oneof_list} =
-      proto_mod.__message_props__()
+    %Protobuf.MessageProps{field_props: props, oneof: oneof_list} = proto_mod.__message_props__()
 
     oneof_map =
       oneof_list
@@ -54,11 +162,12 @@ defmodule Mix.Tasks.GenTestcases do
           prop_name == name_atom
         end)
 
-      prop_val = if is_nil(prop) do
-        gen_prop_val(prop_name, props, oneof_map)
-      else
-        gen_prop_val(prop)
-      end
+      prop_val =
+        if is_nil(prop) do
+          gen_prop_val(prop_name, props, oneof_map)
+        else
+          gen_prop_val(prop)
+        end
 
       Map.put(proto_struct, prop_name, prop_val)
     end)
