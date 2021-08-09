5d54254702a537ef208a9d1460d4d8be5a63d92c
drownbes <drownbes@gmail.com>
2021-08-09 14:09:37 +0300

    WIP

--- nix/pkg-signable-haskell-protoc.nix
+++ nix/pkg-signable-haskell-protoc.nix
@@ -1,7 +1,6 @@
-{ mkDerivation, base, bytestring, Cabal, casing, containers, ghc
+{ mkDerivation, base, bytestring, casing, containers, filepath, ghc
 , ghc-paths, ghc-source-gen, hpack, lens-family, proto-lens
-, proto-lens-protoc, proto-lens-runtime, proto-lens-setup, stdenv
-, text
+, proto-lens-protoc, proto-lens-runtime, stdenv, text
 }:
 mkDerivation {
   pname = "signable-haskell-protoc";
@@ -9,13 +8,12 @@ mkDerivation {
   src = ./../haskell/signable-haskell-protoc;
   isLibrary = false;
   isExecutable = true;
-  setupHaskellDepends = [ base Cabal proto-lens-setup ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    base bytestring casing containers ghc ghc-paths ghc-source-gen
-    lens-family proto-lens proto-lens-protoc proto-lens-runtime text
+    base bytestring casing containers filepath ghc ghc-paths
+    ghc-source-gen lens-family proto-lens proto-lens-protoc
+    proto-lens-runtime text
   ];
-  executableToolDepends = [ proto-lens-protoc ];
   prePatch = "hpack";
   homepage = "https://github.com/coingaming/signable#readme";
   description = "Deterministic serialisation and signatures with proto-lens support";
