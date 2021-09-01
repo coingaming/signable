90d633dff8ff11aabf35f6f2456d3ecb449cdbcf
drownbes <drownbes@gmail.com>
2021-09-01 15:02:26 +0300

    regenerate

--- nix/pkg-signable-haskell-protoc.nix
+++ nix/pkg-signable-haskell-protoc.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, bytestring, casing, containers, filepath, ghc
+{ mkDerivation, base, bytestring, casing, containers, ghc
 , ghc-paths, ghc-source-gen, hpack, lens-family, proto-lens
 , proto-lens-protoc, proto-lens-runtime, stdenv, text
 }:
@@ -10,9 +10,8 @@ mkDerivation {
   isExecutable = true;
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    base bytestring casing containers filepath ghc ghc-paths
-    ghc-source-gen lens-family proto-lens proto-lens-protoc
-    proto-lens-runtime text
+    base bytestring casing containers ghc ghc-paths ghc-source-gen
+    lens-family proto-lens proto-lens-protoc proto-lens-runtime text
   ];
   prePatch = "hpack";
   homepage = "https://github.com/coingaming/signable#readme";
